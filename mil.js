f0b03471b490f673a8861cb9d5840625440c6726
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-09-12 18:23:23 +0100

    i added answer-box

--- mil.js
+++ mil.js
@@ -1,3 +1,7 @@
+const a = document.getElementById("a");
+myElement.style.color = "red";
+const b = document.getElementById("b");
+myElement.style.color = "blue";
 
 function submit(){
     alert("correct")
