49afa795dbbac6b59dfafdfbbd9cdbf2f5409881
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-09-20 12:12:03 +0100

    I added questions

--- mil.js
+++ mil.js
@@ -1,20 +1,77 @@
-// const a = document.getElementById("a");
-// myElement.style.color = "red";
-// const b = document.getElementById("b");
-// myElement.style.color = "blue";
 
-// function submit(){
-//     alert("correct")
-// }
-function dispalyItem (itemArray) {
-    let itemHTML = ""
-  itemArray.map((item, index) => {
-  itemHTML +=`<div class="row bg-light mb-3 rounded">
-    <div class="col-12 col-md-3">${item.item_name}</div>
-    <div class="col-12 col-md-3">${item.item_qty}</div>
-    <div class="col-12 col-md-3">${item.item_price}</div>
-    <div class="col-12 col-md-3">${item.total_price}</div>
-    </div>`
-  })
-  itemDisplay.innerHTML = itemHTML;
-  }
+	let games =  [
+		
+		        {
+		            question : "In what children's game are participants chased by someone designated \"It\"?",
+		            content : [
+		                "Tag",
+		                "Simon Says",
+		                "Charades",
+		                "Hopscotch"
+		            ],
+		            correct : 0
+		        },
+		        {
+		            question : "On a radio, stations are changed by using what control?",
+		            content : [
+		                "Tuning",
+		                "Volume",
+		                "Bass",
+		                "Treble"
+		            ],
+		            correct : 0
+		        },
+		        {
+		            question : "Which material is most dense?",
+		            content : [
+		                "Silver",
+		                "Styrofoam",
+		                "Butter",
+		                "Gold"
+		            ],
+		            correct : 3
+		        },
+		        {
+		            question : "Which state in the United States is largest by area?",
+		            content : [
+		                "Alaska",
+		                "California",
+		                "Texas",
+		                "Hawaii"
+		            ],
+		            correct : 0
+		        },
+				{
+				    correct: 3,
+				    content  : [
+				        "developed the telescope",
+				        "discovered four satellites of Jupiter",
+				        "discovered that the movement of pendulum produces a regular time measurement",
+				        "All of the above"
+				    ],
+				    question: "Galileo was an Italian astronomer who"
+				},
+			
+				{
+				    correct: 0,
+				    content : [
+				        "a club or a local sport association for remarkable achievements",
+				        "amateur athlete, not necessarily an Olympian",
+				        "National Olympic Committee for outstanding work",
+				        "None of the above"
+				    ],
+				    question : "Sir Thomas Fearnley Cup is awarded to"
+				},
+				{
+				    correct: 1,
+				    content : [
+				        "1968",
+				        "1929",
+				        "1901",
+				        "1965"
+				    ],
+				    question : "Oscar Awards were instituted in"
+				}
+            ]
+
+
