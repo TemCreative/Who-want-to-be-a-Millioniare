19db7c6be0e65a721b7e9abafa1f3b6ba64e8f86
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-09-23 13:22:59 +0100

    i added option

--- mil.js
+++ mil.js
@@ -1,22 +1,32 @@
 var questionBanks = [
 	{
-		question :  '',
-		option : ['','','',''],
-		answer : '' 
+		question :  'Free surface of a liquid behaves like a sheet and tends to contract to the smallest possible area due to the',
+		option : ['force of adhesion','force of friction','centrifugal force','force of cohesion'],
+		answer : 'force of cohesion' 
 	},
 	{
-		question :  '',
-		option : ['','','',''],
-		answer : '' 
+		question :  'GNLF stands for',
+		option : ['Gorkha National Liberation Front','Gross National Liberation Form','Both option A and B','None of the above'],
+		answer : 'Gorkha National Liberation Front' 
 	},
 	{
-		question :  '',
-		option : ['','','',''],
-		answer : '' 
+		question :  'For safety, the fuse wire used in the mains for household supply of electricity must be made of metal having',
+		option : ['low melting point','high resistance','high melting point','low specific heat'],
+		answer : 'low melting point' 
 	},
 	{
-		question :  '',
-		option : ['','','',''],
+		question :  'During World War I Germany was defeated in the Battle of Verdun on the western front and Romania declared war on the eastern front in the year',
+		option : ['1914 AD','1915 AD','1916 AD','1917 AD'],
+		answer : '1916 AD' 
+	},
+	{
+		question :  'Durand Cup is associated with the game of',
+		option : ['Cricket','Football','Hockey','Volleyball'],
+		answer : 'Football' 
+	},
+	{
+		question :  '20th August is celebrated as',
+		option : ['Earth Day','Sadbhavana Divas','No Tobacco Day',''],
 		answer : '' 
 	},
 	{
